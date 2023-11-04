aae726418343079152652ee847ddafa6fc6fd8f5
VitoriaRossi <vitoria.rossi555@gmail.com>
2023-11-04 20:04:26 -0300

    Adicionei o nome do time utilizando as cores dele.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.White;
+Console.Write("Corin");
+Console.BackgroundColor = ConsoleColor.White;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.Write("thians.");
+Console.ResetColor();
